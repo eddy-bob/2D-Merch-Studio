4e1944c630d0cc959e02e5e8603c237977b0eb22
Eddy Madu <eddymadu@Eddys-MacBook-Pro.local>
2026-01-13 00:00:44 +0100

    chore: updated the build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["merch-design-enhancer"],
+  serverExternalPackages: ["merch-design-enhancer"],
 };
 
 export default nextConfig;
